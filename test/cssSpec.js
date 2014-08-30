387358a35da266bf6c66c791d8d42f56798c475c
Josh Black <joshuatblack@ufl.edu>
2014-08-30 14:50:10 -0400

    Updating comment-blocks testing spec

--- test/cssSpec.js
+++ test/cssSpec.js
@@ -1,21 +1,56 @@
+'use strict';
+
+let fs = require('fs');
+let path = require('path');
+let rules = require('../lib/rules');
+let assert = require('chai').assert;
+
 describe('CSS', function() {
     describe('#comment-blocks()', function() {
-        it('should throw an error for incorrect inline-style comment', function() {
+        it('should return false for an invalid inline-style comment', function(done) {
+            let file = path.resolve('test/mock-css/invalid-inline-comment.scss');
 
+            rules.check(file, 'comment_blocks').then(function(valid) {
+                assert.isFalse(valid);
+                done();
+            });
         });
 
-        it('should throw an error for incorret block-style comments', function() {
+        it('should return false for invalid block-style comments', function(done) {
+            let file = path.resolve('test/mock-css/invalid-block-comment.scss');
 
+            rules.check(file, 'comment_blocks').then(function(valid) {
+                assert.isFalse(valid);
+                done();
+            });
+        });
+
+        it('should return true for a valid inline-style comment', function(done) {
+            let file = path.resolve('test/mock-css/valid-inline-comment.scss');
+
+            rules.check(file, 'comment_blocks').then(function(valid) {
+                assert.isTrue(valid);
+                done();
+            });
+        });
+
+        it('should return true for a valid block-style comment', function() {
+            let file = path.resolve('test/mock-css/valid-block-comment.scss');
+
+            rules.check(file, 'comment_blocks').then(function(valid) {
+                assert.isTrue(valid);
+                done();
+            });
         });
     });
 
-    describe('#syntax()', function() {
-        it('should throw an error if options have syntax_scss enabled and the file is not .scss', function() {
+    // describe('#syntax()', function() {
+    //     it('should throw an error if options have syntax_scss enabled and the file is not .scss', function() {
 
-        });
+    //     });
 
-        it('should throw an error if options have syntax_less enabled and the file is not .less', function() {
+    //     it('should throw an error if options have syntax_less enabled and the file is not .less', function() {
 
-        });
-    });
+    //     });
+    // });
 });
--- tests.md
+++ tests.md
@@ -5,11 +5,19 @@
     - [x] Need to account for block
 2. [x] Space between property name and value
 3. [ ] Use space between selector and `{`
+    - check for \s{ or \S{
 4. [x] Use `.scss` syntax
 5. [x] Use `.less` syntax
 6. [ ] Use hyphens when naming mixins, extends, classes, functions, and variables
 7. [ ] Blank line above selector that has styles
 8. [ ] Use double quotation marks
+    - Look for ''s
 9. [ ] Use only lowercase, including colors
 10. [ ] Don't have unit specification after zero
+    - check for 0\s or 0\S
 11. [ ] Use a leading zero in decimal numbers `0.5` vs `.5`
+    - check for .[0-9]
+
+
+TODO:
+Suppress console messages when in TEST mode
